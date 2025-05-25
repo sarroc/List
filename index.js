f4ebb5c012550681baf8e0225eb1caa14e6f427c
Sarah Rocco <sarahrocco90@gmail.com>
2025-05-25 10:17:29 +0200

    add command line argument, join paths

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+// use file as executable ^
+
 
 // const counterObject = require('./myscript.js');
 
@@ -12,22 +14,28 @@
 // console.log(newCounterObject.getCounter());
 
 
+// Modules used inside the Node.js library
 const fs = require('fs');
 const util = require('util');
 const chalk = require('chalk');
+const path = require('path');
+
 
 // Method #2  (promisify function)
 //  const lstat = util.promisify(fs.lstat);
 
-// Method #3 (best solution, no downsides as everything is executed simultaneously)
+
+// Method #3 (Promise.all = best solution, no downsides as everything is executed simultaneously)
 const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
     if (err) {
         console.log(err);
     }
     const statPromises = filenames.map(filename =>{
-        return lstat(filename);
+        return lstat(path.join(targetDir, filename));
     });
 
     const allStats = await Promise.all(statPromises);
@@ -56,3 +64,8 @@ fs.readdir(process.cwd(), async (err, filenames) => {
 //         });
 //     }); 
 // };
+
+
+// Files UP one directory: ls ../
+// Files FROM ROOT DIRECTORY ls /
+// Files FROM HOME DIRECTORY ls ~/
